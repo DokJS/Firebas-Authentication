9a2a887affa2c7089e4f719af91e5f7b1cb6ab58
DokJS <omarmbengue6919@gmail.com>
2022-05-30 17:05:14 +0000

    commit test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React from 'react';
 function App() {
   return (
     <div className="app">
-     <div className="text-center bg-gray-300">Hello Tailwind</div>
+      <div className="text-center bg-gray-300">Hello Tailwind</div>
+      <h1 className="text-center">Hello Tailwind</h1>
     </div>
   );
 }
