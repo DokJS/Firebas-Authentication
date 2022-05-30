1e427d5c5fa847e488c52391f3404957a31f3664
DokJS <omarmbengue6919@gmail.com>
2022-05-30 14:57:13 +0000

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+     <div className="text-center bg-gray-300">Hello Tailwind</div>
+    </div>
+  );
+}
+
+export default App;
